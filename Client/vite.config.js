029fd769417c5c4cc8ca95c4f110c3b4aff6523b
Ditrex24 <vasquezsergiodg@gmail.com>
2023-10-19 22:39:57 -0400

    vite

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -14,10 +14,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     include: ['@fortawesome/fontawesome-svg-core'],
   },
+  resolve: {
+    alias: {
+      '@': '/src',  // Puedes ajustar la ruta según la estructura de tu proyecto
+    },
+    extensions: ['.mjs', '.js', '.jsx', '.ts', '.tsx', '.json'], // Agrega extensiones de archivo
+  },
 });
+
+
+
+
