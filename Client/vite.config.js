f4e7bcce8187a85cce85ac1a81fe19f2ae097d19
Ditrex24 <vasquezsergiodg@gmail.com>
2023-10-19 22:32:17 -0400

    Deploy4

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-})
-
-//--------------------USAR PARA DEPLOY-------------------//
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // // https://vitejs.dev/config/
 // export default defineConfig({
+
 //   plugins: [react()],
-//   optimizeDeps: {
-//     include: ['@fortawesome/fontawesome-svg-core'],
+//   server: {
+//     port: 3000,
 //   },
-// });
+// })
+
+//--------------------USAR PARA DEPLOY-------------------//
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    include: ['@fortawesome/fontawesome-svg-core'],
+  },
+});
